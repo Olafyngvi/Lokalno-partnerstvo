5994115b9de99e492889e79074870efc9aff4f59
Rijad Dzanko <rijadda@gmail.com>
2020-07-30 13:05:20 +0200

    gdpr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  cookieMessage: any = 'This website uses cookies to ensure you get the best experience on our website.';
+  cookieDismiss: any = 'Got it';
+  cookieLinkText: any = 'Learn more';
   title = 'Obrazovanje-odraslih';
+  ngOnInit(): void {
+    const cc = window as any;
+    cc.cookieconsent.initialise({
+      palette: {
+        popup: {
+          background: '#164969'
+        },
+        button: {
+          background: '#ffe000',
+          text: '#164969'
+        }
+      },
+      theme: 'classic',
+      content: {
+        message: this.cookieMessage,
+        dismiss: this.cookieDismiss,
+        link: this.cookieLinkText,
+        // href: environment.Frontend + '/dataprivacy'
+      }
+    });
+  }
 }
